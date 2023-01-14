b2bd4ee47cf51b108a7792d5ea790d30cb2cf7a7
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2023-01-14 22:17:21 +0600

    navbar and footer updated

--- src/Router/Routes.js
+++ src/Router/Routes.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import About from "../pages/About/About";
 import Homepage from "../pages/Homepage/Homepage";
+import Profile from "../pages/Profile/Profile";
 
 
 
@@ -13,6 +15,16 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Homepage />,
+
+            },
+            {
+                path: '/about',
+                element: <About />,
+
+            },
+            {
+                path: '/profile',
+                element: <Profile />,
 
             },
 
